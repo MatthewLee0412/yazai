24866a838687a6baf8f9744b9edf018c88e35f1d
liyu <liyu@bkqtech.com>
2026-06-07 15:10:42 +0800

    Initial commit

---
+++ src/main/java/com/bkq/yazai/YazaiApplication.java
@@ -0,0 +1,13 @@
+package com.bkq.yazai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YazaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YazaiApplication.class, args);
+    }
+
+}
